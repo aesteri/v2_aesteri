433306d8f469b76a98a51310da56d8fe08effa92
Christine <132790780+aesteri@users.noreply.github.com>
2025-05-07 17:49:36 -0400

    styled

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,8 @@
-// // @ts-check
-// import { defineConfig } from 'astro/config';
-// import react from '@astrojs/react';
-
-
-// // https://astro.build/config
-// export default defineConfig({
-//   integrations: [react()],
-//   vite: {
-//     resolve: {
-//         alias: {
-//             '@': '/src',
-//             '@/components': '/src/components',
-//             '@/css':'/src/styles',
-//         }
-//     },
-//     optimizeDeps: {
-//         include: ["@tabler/icons-react"],
-//       },
-//   }
-// });
-
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import path from 'node:path'; // Import the 'path' module
-import { fileURLToPath } from 'node:url'; // For ES module __dirname equivalent
+import path from 'node:path'; 
+import { fileURLToPath } from 'node:url'; 
 import copy from 'rollup-plugin-copy';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -40,7 +18,6 @@ export default defineConfig({
   vite: {
     plugins: [
       copy({
-        // Copy only on first build. We dont want to trigger additional server reloads.
         copyOnce: true,
         hook: 'buildStart',
         targets: [
@@ -50,9 +27,9 @@ export default defineConfig({
     ],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'), // Use path.resolve
-        '@components': path.resolve(__dirname, './src/components'), // Consistent naming
-        '@styles': path.resolve(__dirname, './src/styles'), // Use path.resolve
+        '@': path.resolve(__dirname, './src'), 
+        '@components': path.resolve(__dirname, './src/components'), 
+        '@styles': path.resolve(__dirname, './src/styles'), 
       }
     },
   }
